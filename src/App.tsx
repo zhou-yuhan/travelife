605753224b1a23e6b8b3f7f7faf4257b871f2d6d
zhouyuhan <zhouyuhan_@outlook.com>
2024-02-26 15:00:06 +0800

    dev: split canvas into two panes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Appbar } from './appbar';
-import { Grid } from '@mui/material';
+import { Grid, Paper } from '@mui/material';
+import Box from '@mui/material/Box';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from '@mui/material/styles';
 import { themeDefault, themeGanyu, themeGrey } from './theme';
+import { makeStyles } from '@mui/styles';
+import SplitPane, { Pane } from 'split-pane-react';
+import 'split-pane-react/esm/themes/default.css'
 
 function App() {
+    const [sizes, setSizes] = useState([
+        500, // TODO: set default size
+        '50%',
+        '50%',
+    ]);
+
     return (
         <ThemeProvider theme={themeGanyu}>
-            <BrowserRouter>
+            <Box
+                sx={{
+                    width: '100vw', height: '100vh', display: 'flex', flexDirection: 'column'
+                }}
+            >
                 <Appbar />
-                <Grid item xs={12}
-                    md={10}
-                    lg={9}
-                    xl={8}
-                    style={{ margin: "auto" }}>
-                </Grid>
-            </BrowserRouter>
+                <SplitPane
+                    sashRender={() => null}
+                    split='vertical'
+                    sizes={sizes}
+                    onChange={setSizes}
+                    resizerSize={10}
+                >
+                    <Pane minSize={'10%'} maxSize='90%'>
+                        <Paper sx={{ height: '100%' }} variant='outlined'>
+                            pane1
+                        </Paper>
+                    </Pane>
+                    <Paper sx={{ height: '100%' }} variant="outlined">
+                        pane2
+                    </Paper>
+                </SplitPane>
+            </Box>
         </ThemeProvider>
     )
 }
